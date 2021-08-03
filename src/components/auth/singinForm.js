6c30a1f6fdd493221febc7e63560b29fe206a513
Ezequiel H <horvitzezequiel@gmail.com>
2021-08-03 17:11:09 +0200

    added details.js, page title and form-fields styles

--- src/components/auth/singinForm.js
+++ src/components/auth/singinForm.js
@@ -3,13 +3,33 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
+import Details from '../details';
+
+import history from '../../history';
 
 class SignInForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+            {
+                _id: 0,
+                title: 'Not registered? Create account here',
+                onClick: () => history.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot account email?',
+                onClick: () => console.log('forgot email')
+            },
+            {
+                _id: 2,
+                title: 'Forgot password?',
+                onClick: () => console.log('forgot password')
+            }
+        ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-               <Field className='sign-in-form__email'
+                <Field className='sign-in-form__email'
                 type='email'
                 title='Email'
                 placeholder='Email'
@@ -21,12 +41,14 @@ class SignInForm extends Component {
                 placeholder='Password'
                 name='password'
                 component={FormInput}/>
+                <div className='sign-in-form__line'></div>
                 <Field className='sign-in-form__login'
                 onClick={() => console.log('tryna submit')}
                 type='submit'
                 title='Login'
                 name='login'
-        component={FormButton}/>
+                component={FormButton}/>
+                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
             </form>
         )
     }
